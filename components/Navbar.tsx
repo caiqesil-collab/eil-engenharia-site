adb31992675dc874608baf7b23cbd1cdd2ae755d
caiqesil-collab <caiqe.sil@gmail.com>
2025-12-23 00:34:05 -0300

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <header style={{ padding: "16px", background: "#111", color: "#fff" }}>
+      <strong>EIL Engenharia</strong>
+    </header>
+  );
+}
